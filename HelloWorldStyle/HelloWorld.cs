c6715e84a250ee3eed372714a77496c5e2527239
Inessa Abramian <inessa_abramyan@epam.com>
2021-06-29 13:11:40 +0000

    hello-world-styleUpdate README.md

--- HelloWorldStyle/HelloWorld.cs
+++ HelloWorldStyle/HelloWorld.cs
@@ -1,40 +1,27 @@
 ﻿namespace HelloWorldStyle
 {
-    public static
-        
-        class HelloWorld {
-
-        public static string ReturnGreeting(string hello, string addressee) {
-            return hello + ", " + addressee + "!"; }
-
-        public   static   int   ReturnSum   (   int   a   ,   int   b  )
+    public static class HelloWorld
+    {
+        public static string ReturnGreeting(string hello, string addressee)
         {
-            return 
-                a
-                +    b;
+            return hello + ", " + addressee + "!";
+        }
+
+        public static int ReturnSum(int a, int b)
+        {
+            return a + b;
         }
 
         public static int[] ReturnArray(int length)
         {
-            var   array   =  new
-                int
-                [
-                    length
-                ];
+            var array = new int[length];
 
-            for
-            (
-                int i = 0 ;   i   <   length ; i ++
-            )
+            for (int i = 0; i < length; i++)
             {
-                array [i] = i ;
+                array[i] = i;
             }
 
-            return   array;
+            return array;
         }
-
-
     }
-
-
 }
